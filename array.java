1d0880b87b9eeab23798a708766f5ba3b35ce403
RAJESHREE-K <skathiravanraje@gmail.com>
2024-08-02 23:52:40 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sum{
+    public static void main(String []arg){
+        Scanner sc=new Scanner(System.in);
+        int n,a,sum;
+        for(int i=10;i<=20;i++)
+        {
+            n=i/10;
+            a=i%10;
+            sum=a+n;
+            System.out.println(sum);
+        }
+
+        
+    }
+}
